a707e4810cea435db0d355edf4a72bca8d42246a
Distant-One <kmarr41@gmail.com>
2018-12-18 16:59:13 -0500

    Started on intel hex file i/o

---
+++ intelhex.c
@@ -0,0 +1,64 @@
+/** 
+* @file intelhex.c
+* @brief
+* @details 
+* @version
+* @date Tue 18 Dec 2018 04:58:07 PM EST
+* @author 
+* @copyright The GNU General Public License
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+*
+*
+*
+*	Intel hex file format bsed on https://en.wikipedia.org/wiki/Intel_HEX
+*
+*	Each line contains teh following fields
+*	Start Code: ASCII Character 0: 1 character, Alwas an ASCII :
+*	Byte Count: ASCII Characters 1,2: 2 characters, representing number 
+*		of bytes (pairs of ascii characters in the data fields
+*	Address: ASCII Characters 3,4,5,6: 4 characters, representing four hex digits, 
+*		representing the 16-bit beginning memory address offset of the data. 
+*		The physical address of the data is computed by adding this offset to 
+*		a previously established base address, thus allowing memory addressing 
+*		beyond the 64 kilobyte limit of 16-bit addresses. The base address, 
+*		which defaults to zero, can be changed by various types of records. 
+*		Base addresses and address offsets are always expressed as big endian values.
+*	Record Type: ASCII Characters 7,8: representing two hex digits, 00 to 05, defining the meaning of the data field.
+*		00: Data
+*		01: End of File
+*		02: Extended Segment Address
+*		04: Extended Linear Address
+*		05: Start Linear Address 
+*	Data: ASCII characters 9-2n: a sequence of n bytes of data, represented by 2n (ASCII) hex digits.
+*	Checksum: ASCII Chatacters 2n+1 through 2n+2, two ascii characters representing two hex digits, 
+*		a computed value that can be used to verify the record has no errors.
+*		Checksum calculation:
+*		A record's checksum byte is the two's complement (negative) of the least significant byte (LSB) 
+*		of the sum of all decoded byte values in the record preceding the checksum. 
+*		It is computed by summing the decoded byte values and extracting the LSB of the sum 
+*		(i.e., the data checksum), and then calculating the two's complement of the LSB i
+*		(e.g., by inverting its bits and adding one). 
+*	
+*/
+
+/* --- include files --- */
+
+/* --- defines and constants --- */
+
+/* --- global variables --- */
+
+/* --- function prototypes --- */
+
+/* --- main --- */
+
+/* --- functions --- */
---
+++ intelhex.h
@@ -0,0 +1,31 @@
+/** 
+* @file intelhex.h
+* @brief
+* @details 
+* @version
+* @date Tue 18 Dec 2018 04:33:02 PM EST
+* @author 
+* @copyright The GNU General Public License
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
+
+/* --- include files --- */
+
+/* --- defines and constants --- */
+
+/* --- global variables --- */
+
+/* --- function prototypes --- */
+
+/* --- main --- */
+
+/* --- functions --- */
